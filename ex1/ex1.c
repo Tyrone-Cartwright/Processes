2381cf4bf53ce9fb62ac56b31d0cf33de2d05a0d
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-05-01 19:40:19 -0400

    Finished ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
     // Your code here
+    int x = 100;
+
+    printf("X value before fork: %d, (pid: %d) \n", x, getpid());
+
+    int new_fork = fork();
+
+    if (new_fork < 0)
+    {
+        // fork failed
+        fprintf(stderr, "Fork Failed!\n");
+        exit(1);
+    }
+    else if (new_fork == 0)
+    {
+        // fork successful
+        printf("Child here (pid: %d), (X: %d) \n", (int)getpid(), x);
+        x = 150;
+        printf("Child, X value is now: %d \n", x);
+    }
+    else
+    {
+        int wc = waitpid(new_fork, NULL, 0);
+        printf("Parent here (pid: %d), (X: %d) \n", (int)getpid(), x);
+        x = 125;
+        printf("Parent, X value is now: %d \n", x);
+    }
+
+    printf("X value after fork: %d, (pid: %d) \n", x, getpid());
 
     return 0;
 }
