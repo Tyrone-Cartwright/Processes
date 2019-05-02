fa423cf7fd7899a77d59703c5e35b8471c27c073
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-05-02 17:14:10 -0400

    Finished ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,7 +1,9 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
+
+// Which ever one you use depends on how many args you need in the fn call
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,7 +12,18 @@
 
 int main(void)
 {
-    // Your code here    
+    // Your code here
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        char *args[] = {"ls", "-1", NULL};
+        execv("/bin/ls", args);
+    }
+    else
+    {
+        printf("This is the parent process\n");
+    }
 
     return 0;
 }
